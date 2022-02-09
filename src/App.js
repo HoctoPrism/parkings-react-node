e7d3121ef3c281582f8f36d3a51ef9b5f125ed40
unknown <nemexxix@gmail.com>
2022-02-09 17:29:42 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react/cjs/react.development';
 import './App.css';
 
-function App() {
+function App (){
+
+  document.title = "First";
+
+  let parkings = "parkings";
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://127.0.0.1:7000/parkings`)
+      .then((response) => response.json())
+      .then((actualData) => {
+        setData(actualData);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Liste des {parkings}</h1>
+      <ul>{data && data.map(({ id, name, city, type }) => (
+          <li key={id} className="card">
+            <div>{id}</div>
+            <div>{name}</div>
+            <div>{type}</div>
+            <div>{city}</div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/server/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const parkings = require('./parkings.json')
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();});
+
+app.get('/parkings', (req,res) => {
+    res.status(200).json(parkings)
+})
+
+app.get('/parkings/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    const parking = parkings.find(parking => parking.id === id)
+    res.status(200).json(parking)
+    res.status(400).send('parking non trouvé :(')
+})
+
+app.post('/parkings', (req,res) => {
+    parkings.push(req.body)
+    res.status(200).json(parkings)
+})
+
+app.put('/parkings/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    let parking = parkings.find(parking => parking.id === id)
+    parking.name = req.body.name,
+    parking.city = req.body.city,
+    parking.type = req.body.type,
+    res.status(200).json(parking)
+})
+
+app.delete('/parkings/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    let parking = parkings.find(parking => parking.id === id)
+    parkings.splice(parkings.indexOf(parking),1)
+    res.status(200).json(parkings)
+})
+
+app.listen(7000, () => {
+    console.log('Serveur à l\'écoute')
+})
