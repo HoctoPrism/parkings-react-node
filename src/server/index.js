851ee9927e5c08972592343345361679b104c303
unknown <nemexxix@gmail.com>
2022-02-09 23:52:58 +0100

    POST/PATCH/DELETE now modify the parking json file

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const parkings = require('./parkings.json')
+const endpoint = "./src/server/parkings.json"
 
 app.use(
     function (req, res, next) {
@@ -25,23 +27,39 @@ app.get('/parkings/:id', (req,res) => {
 })
 
 app.post('/parkings', (req,res) => {
-    parkings.push(JSON.parse(JSON.stringify(req.body)))
+    fs.readFile(endpoint, function (err, data) {
+        var json = JSON.parse(data);
+        json.push(JSON.parse(JSON.stringify(req.body)))
+        fs.writeFile(endpoint, JSON.stringify(json, null, 2), function(err, result) {
+            if(err) console.log('error', err);
+        });
+    })
     res.status(200).json(parkings)
 })
 
 app.patch('/parkings/:id', (req,res) => {
     const id = parseInt(req.params.id)
-    let parking = parkings.find(parking => parking.id === id)
-    parking.name = req.body.name,
-    parking.city = req.body.city,
-    parking.type = req.body.type,
-    res.status(200).json(parking)
+    const foundIndex = parkings.findIndex(parking => parking.id === id)
+    fs.readFile(endpoint, function (err, data) {
+        var json = JSON.parse(data);
+        json[foundIndex] = JSON.parse(JSON.stringify(req.body))
+        fs.writeFile(endpoint, JSON.stringify(json, null, 2), function(err, result) {
+            if(err) console.log('error', err);
+        });
+    }) 
+    res.status(200).json(parkings)
 })
 
 app.delete('/parkings/:id', (req,res) => {
     const id = parseInt(req.params.id)
-    let parking = parkings.find(parking => parking.id === id)
-    parkings.splice(parkings.indexOf(parking),1)
+    const foundIndex = parkings.findIndex(parking => parking.id === id)
+    fs.readFile(endpoint, function (err, data) {
+        var json = JSON.parse(data);
+        json.splice(foundIndex, 1);
+        fs.writeFile(endpoint, JSON.stringify(json, null, 2), function(err, result) {
+            if(err) console.log('error', err);
+        });
+    }) 
     res.status(200).json(parkings)
 })
 
